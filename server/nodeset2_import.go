53b2236965620ca4f9a2f5fc56957c5816ad7db0
Isak Styf <isak.styf@masarinconsulting.se>
2026-01-13 17:12:42 +0100

    fix proper namespace id lookup key

--- server/nodeset2_import.go
+++ server/nodeset2_import.go
@@ -53,7 +53,7 @@ func (srv *Server) namespacesImportNodeSet(nodes *schema.UANodeSet) (*nsIDLookup
 			ns = NewNodeNameSpace(srv, name)
 		}
 
-		nameSpaceLookup[uint16(i)] = ns.ID()
+		nameSpaceLookup[uint16(i+1)] = ns.ID()
 	}
 
 	return &nameSpaceLookup, nil
